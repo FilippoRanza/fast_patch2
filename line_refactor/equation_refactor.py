a29b019def76739424389a1ad35a12031db2560d
Filippo <filipporanza@gmail.com>
2018-11-29 12:00:37 +0100

    improved parenthesis refactor and adjusted license

--- line_refactor/equation_refactor.py
+++ line_refactor/equation_refactor.py
@@ -19,33 +19,25 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-#
-#  fast_patch - Automatically refactor Latex code
-#
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 
 import re
 
 _begin_regex_ = re.compile(r'\\begin\{(align|equation)\*?\}')
 _end_regex_ = re.compile(r'\\end\{(align|equation)\*?\}')
 
-_parenthesis_refactor_ = [(re.compile(r'(\\left)?\('), r'\\left('),
-                          (re.compile(r'(\\left)?\['), r'\\left['),
-                          (re.compile(r'(\\right)?\)'), r'\\right)'),
-                          (re.compile(r'(\\right)?\]'), r'\\right]')]
+
+def refactor_parenthesis(match):
+    o, p = match.groups()
+    if o is None:
+        if p == '(' or p == '[':
+            o = r'\left'
+        else:
+            o = r'\right'
+
+    return f'{o}{p}'
+
+
+_equation_refactor_ = [(re.compile(r'(\\left|\\right)?(\(|\)|\[|\])'), refactor_parenthesis)]
 
 
 class EquationRefactor:
@@ -55,7 +47,7 @@ class EquationRefactor:
 
     @staticmethod
     def _refactor_(line):
-        for r, s in _parenthesis_refactor_:
+        for r, s in _equation_refactor_:
             line = r.sub(s, line)
         return line
 
