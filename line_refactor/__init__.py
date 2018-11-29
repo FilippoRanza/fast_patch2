d1e473202675938ed1020c1f83983f10df959019
Filippo Ranza <filipporanza@gmail.com>
2018-11-29 23:44:33 +0100

    correct licence header

--- line_refactor/__init__.py
+++ line_refactor/__init__.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
 #
-#  fast_patch - Automatically refactor Latex code
+#  fast_patch2 - Automatically refactor Latex code
 #
 #  Copyright (c) 2018 Filippo Ranza <filipporanza@gmail.com>
 #
@@ -19,25 +19,6 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-#
-#  fast_patch2 - Automatically refactor Latex code
-#
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-
 
 from .line_processor import refactor_line, reset_refactor
-
 
